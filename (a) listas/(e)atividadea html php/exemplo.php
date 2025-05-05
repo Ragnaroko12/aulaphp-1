6d122818e5ad85101b64c9abdfcefad4923dbcb1
Ragnaroko12 <luisgustsvo5@gmail.com>
2025-05-05 10:50:46 -0300

    macaco2.0

--- (a) listas/(e)atividadea html php/exemplo.php
+++ (a) listas/(e)atividadea html php/exemplo.php
@@ -17,100 +17,148 @@ session_start();
 
         <h1>fixinha da associação Pato</h1>
         <form action="soma.php" method="post" class="row">
-        <input class="form-control" type="text" name="nome" id="" placeholder="nome">
-        <h3> endereço</h3>
+            <!-- linha 1 -->
+            <div class="row">
+                <div class="col-mb-12 mt-2">
+                    <input class="form-control" type="text" name="nome" id="" placeholder="nome">                    
+                </div>
 
-        <div class="col-md-6">
-            <input class="form-control" type="text" name="rua" id="" placeholder="Rua"><br>
-            
-        </div>
-        <div class="col-md-6">
-            <input class="form-control" type="number" name="numero" id="" placeholder="Numero da casa"><br>
-        </div>
-        
-        <div class="col-md-6">
-            <input class="form-control" type="text" name="bairro" id="" placeholder="Bairro"><br>
-        </div>
+            </div>
 
-        <div class="col-md-6">
-            <input class="form-control" type="text" name="cep" id="" placeholder="CEP"><br>
-        </div> 
-        <div class="col-md-6">
-            <input class="form-control" type="text" name="cidade" id="" placeholder="Cidade"><br>            
-        </div>
-        <div class="col-md-6">
-            <select class="form-select" id="estado" name="estado">
-            <option value="">selecione o estado</option>
-            <option value="AC">Acre</option>
-            <option value="AL">Alagoas</option>
-            <option value="AP">Amapá</option>
-            <option value="AM">Amazonas</option>
-            <option value="BA">Bahia</option>
-            <option value="CE">Ceará</option>
-            <option value="DF">Distrito Federal</option>
-            <option value="ES">Espírito Santo</option>
-            <option value="GO">Goiás</option>
-            <option value="MA">Maranhão</option>
-            <option value="MT">Mato Grosso</option>
-            <option value="MS">Mato Grosso do Sul</option>
-            <option value="MG">Minas Gerais</option>
-            <option value="PA">Pará</option>
-            <option value="PB">Paraíba</option>
-            <option value="PR">Paraná</option>
-            <option value="PE">Pernambuco</option>
-            <option value="PI">Piauí</option>
-            <option value="RJ">Rio de Janeiro</option>
-            <option value="RN">Rio Grande do Norte</option>
-            <option value="RS">Rio Grande do Sul</option>
-            <option value="RO">Rondônia</option>
-            <option value="RR">Roraima</option>
-            <option value="SC">Santa Catarina</option>
-            <option value="SP">São Paulo</option>
-            <option value="SE">Sergipe</option>
-            <option value="TO">Tocantins</option>
-            <option value="EX">Estrangeiro</option>
-            </select>                
-        </div>
+            <h3> endereço</h3>
+            <!-- linha 2 -->
+            <div class="row">
+                <div class="col-md-6">
+                    <input class="form-control" type="text" name="rua" id="" placeholder="Rua">
+                    
+                </div>
+                <div class="col-md-6">
+                    <input class="form-control" type="number" name="numero" id="" placeholder="Numero da casa">
+                </div>
 
+            </div>
+            <!-- linha 3 -->       
+            <div class="row">
+                <div class="col-md-6 mt-2">
+                    <input class="form-control" type="text" name="bairro" id="" placeholder="Bairro">
+                </div>
+                
+                <div class="col-md-6 mt-2">
+                    <input class="form-control" type="text" name="cep" id="" placeholder="CEP">
+                </div>         
+            </div>
+            <!-- linha 4 -->        
+            <div class="row">
+                <div class="col-md-6 mt-2">
+                    <input class="form-control" type="text" name="cidade" id="" placeholder="Cidade">           
+                </div>
+                <div class="col-md-6 mt-2">
+                    <select class="form-select" id="estado" name="estado">
+                    <option value="">selecione o estado</option>
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="AM">Amazonas</option>
+                    <option value="BA">Bahia</option>
+                    <option value="CE">Ceará</option>
+                    <option value="DF">Distrito Federal</option>
+                    <option value="ES">Espírito Santo</option>
+                    <option value="GO">Goiás</option>
+                    <option value="MA">Maranhão</option>
+                    <option value="MT">Mato Grosso</option>
+                    <option value="MS">Mato Grosso do Sul</option>
+                    <option value="MG">Minas Gerais</option>
+                    <option value="PA">Pará</option>
+                    <option value="PB">Paraíba</option>
+                    <option value="PR">Paraná</option>
+                    <option value="PE">Pernambuco</option>
+                    <option value="PI">Piauí</option>
+                    <option value="RJ">Rio de Janeiro</option>
+                    <option value="RN">Rio Grande do Norte</option>
+                    <option value="RS">Rio Grande do Sul</option>
+                    <option value="RO">Rondônia</option>
+                    <option value="RR">Roraima</option>
+                    <option value="SC">Santa Catarina</option>
+                    <option value="SP">São Paulo</option>
+                    <option value="SE">Sergipe</option>
+                    <option value="TO">Tocantins</option>
+                    <option value="EX">Estrangeiro</option>
+                    </select>                
+                </div>
+            </div>
+            <!-- linha 5 -->
+            <div class="row">
+                <div class="col-mb-12 mt-4">
+                    <input class="form-control" type="email" name="email" id="" placeholder="Email">                    
+                </div>
+            </div>
+            <!-- linha 6 -->
+            <div class="row">
+                <div class="col-md-6 mt-2">
+                    <input class="form-control" type="number" name="cpf" id="" placeholder="CPF">                
+                </div>
 
+                <div class="col-md-6 mt-2">
+                    <input class="form-control" type="number" name="rg" id="" placeholder="RG">                    
+                </div>                
+            </div>
+            <!-- linha 7 -->
+            <div class="row">
+                <div class="col-mb-12 mt-2">
+                    <input class="form-control" type="date" name="nacimento" id="" placeholder="Data de nacimento">                    
+                </div>
+            </div>
+            <!-- linha 8 -->
+            <div class="row">
+                <div class="col-md-6 mt-2">
+                    <select class="form-select" name="civil" id="" placeholder="estado civil">
+                    <option value="">selecione seu estado civil</option>
+                    <option value="solteiro">Solteiro</option>
+                    <option value="casado">Casado</option>
+                    <option value="compremetido">compormetido</option>
+                    <option value="viuva">viuvo(a)</option>
+                    </select>                    
+                </div>
+
+                <div class="col-md-6 mt-2">
+                    <select class="form-select" name="sexo" id="">
+                        <option value="">selecione seu genero</option>
+                        <option value="Homem">Homem</option>
+                        <option value="mulher">mulher</option>
+                        <option value="omniman">omniman</option>
+                    </select>                    
+                </div>
+
+            </div>
+            <!-- linha 9 -->
+            <div class="row">
+                <div class="col-mb-12 mt-2">
+                    <input class="form-control" type="password" name="senha" id="" placeholder="insira a senha">                    
+                </div>
+
+            </div>
+            <!-- linha 10 -->
+            <div class="row">
+                <div class="col-mb-12 mt-2">
+                    <input class="form-control" type="password" name="csenha" id="" placeholder="confirmar a senha">                    
+                </div>
+            </div>
+            <!-- linha 11 -->
+            <div class="row">
+                <div class="col-mb-12 mt-2">
+                    <input class="btn btn-primary" type="submit" value="enviar">                    
+                </div>
+            </div>
         </form>
-        
-        <form action="soma.php" method="post">
-        <br><br>
-        <input class="form-control" type="email" name="email" id="" placeholder="Email"><br>
-        <input class="form-control" type="number" name="cpf" id="" placeholder="CPF"><br>
-        <input class="form-control" type="number" name="rg" id="" placeholder="RG"><br>
-        <input class="form-control" type="date" name="nacimento" id="" placeholder="Data de nacimento"><br>
-        <select class="form-select" name="civil" id="" placeholder="estado civil">
-        <option value="">selecione seu estado civil</option>
-        <option value="solteiro">Solteiro</option>
-        <option value="casado">Casado</option>
-        <option value="compremetido">compormetido</option>
-        <option value="viuva">viuvo(a)</option>
-        </select><br>
-        <select class="form-select" name="sexo" id="">
-            <option value="">selecione seu genero</option>
-            <option value="Homem">Homem</option>
-            <option value="mulher">mulher</option>
-            <option value="omniman">omniman</option>
-        </select><br>
-        <input class="form-control" type="password" name="senha" id="" placeholder="insira a senha"><br>
-        <input class="form-control" type="password" name="csenha" id="" placeholder="confirmar a senha"><br>
-        <input class="btn btn-primary" type="submit" value="enviar">
-        </form>
-    </div>
-    
-    <div class="container">
+
         <?php
-        if (isset($_SESSION)) {
-            $resposta = $_SESSION["Resposta"];
-            echo $resposta;
-        }
+            if (isset($_SESSION["Resposta"])) {
+                $resposta = $_SESSION["Resposta"];
+                echo $resposta;
+            }
 
         ?>          
     </div>
-   
-    
+
 </body>
 </html>
-
--- (a) listas/(e)atividadea html php/soma.php
+++ (a) listas/(e)atividadea html php/soma.php
@@ -3,20 +3,35 @@ session_start();
 
 $senha = $_POST["senha"];
 $csenha = $_POST["csenha"];
+$controle = 0;
 
-if ($_POST !== "") {
+foreach ($_POST as $key => $value) {
+    if ($value == 0) {
+        $controle++;
+    }
+
+}
+
+if ($controle > 0) {
 
     if ($senha == $csenha and $senha !== "") {
-        $_SESSION["Resposta"] = "dados validados";
+        $_SESSION["Resposta"] = "<div class='alert alert-success' role='alert'>
+         dados validados com sucesso que o grande lider esteja com você 
+         </div>";
         header("location:exemplo.php");
     }else{
-        $_SESSION["Resposta"] = "O Valor da senha é nulo ou os campos não batem";
+        $_SESSION["Resposta"] = "<div class='alert alert-danger' role='alert'>
+         O lider desaprova sua estupidez o Valor da senha é nulo ou os campos não batem 
+         </div>";
         header("location:exemplo.php");
     }
 }else{
-    $_SESSION["Resposta"] = "algum valor incompleto";
+    $_SESSION["Resposta"] = "<div class='alert alert-danger' role='alert'>
+    O lider despreza suas ações algum campo não foi preenchido corretamente
+    </div>";
     header("location:exemplo.php");
 }
+
 
 
 ?>
